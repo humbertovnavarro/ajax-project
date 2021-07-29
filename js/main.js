d7d4174bb5b3ec4266df317e2327c1c5ff6f29fa
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-29 11:13:35 -0700

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var $loadModal = document.querySelector('.load-modal');
 var $loadMore = document.querySelector('.load-more');
 var flickity;
 var results;
+
 var options = {
   imagesLoaded: true,
   percentPosition: true,
@@ -14,50 +15,7 @@ var options = {
   pageDots: false
 };
 
-data.decks.push(new Deck('Debugging Deck'));
-
-function getCurrentSearchBox() {
-  if (window.innerWidth > 900) {
-    return $searchBoxes[1];
-  } else {
-    return $searchBoxes[0];
-  }
-}
-
-function resetFlickity() {
-  if (flickity !== undefined) {
-    flickity.destroy();
-  }
-  $search.innerHTML = '';
-  flickity = new Flickity($search, options);
-  flickity.on('staticClick', function (event) {
-    if (event.target.matches('.is-selected')) {
-      data.decks[data.deckIndex].addCard(event.target.getAttribute('data-id'));
-      toggleSearch(false);
-      return;
-    }
-    flickity.select(event.target.getAttribute('data-index'));
-  });
-  flickity.on('select', onSelect);
-}
-
-function onSelect(event) {
-  var zoomed = 'scale(2) translateY(-20px)';
-  if (window.innerWidth < 900) {
-    zoomed = 'scale(1.25) translateY(-20px)';
-  }
-  var normal = 'scale(1) translateY(0px)';
-  for (var i = 0; i < flickity.cells.length; i++) {
-    flickity.cells[i].element.style.transform = normal;
-    flickity.cells[i].element.style.zIndex = ('1');
-  }
-  flickity.selectedElement.style.zIndex = '2';
-  if (flickity.selectedElement.getAttribute('layout') === 'split') {
-    flickity.selectedElement.style.transform = zoomed + ' rotate(90deg)';
-    return;
-  }
-  flickity.selectedElement.style.transform = zoomed;
-}
+data.decks.push(new Deck('Test Deck'));
 
 $searchModal.addEventListener('wheel', function (event) {
   if (event.deltaY > 0) {
@@ -186,4 +144,47 @@ function searchMore() {
     $loadModal.classList.add('hidden');
   };
   xhr.send();
+}
+
+function getCurrentSearchBox() {
+  if (window.innerWidth > 900) {
+    return $searchBoxes[1];
+  } else {
+    return $searchBoxes[0];
+  }
+}
+
+function resetFlickity() {
+  if (flickity !== undefined) {
+    flickity.destroy();
+  }
+  $search.innerHTML = '';
+  flickity = new Flickity($search, options);
+  flickity.on('staticClick', function (event) {
+    if (event.target.matches('.is-selected')) {
+      data.decks[data.deckIndex].addCard(event.target.getAttribute('data-id'));
+      toggleSearch(false);
+      return;
+    }
+    flickity.select(event.target.getAttribute('data-index'));
+  });
+  flickity.on('select', onSelect);
+}
+
+function onSelect(event) {
+  var zoomed = 'scale(2) translateY(-20px)';
+  if (window.innerWidth < 900) {
+    zoomed = 'scale(1.25) translateY(-20px)';
+  }
+  var normal = 'scale(1) translateY(0px)';
+  for (var i = 0; i < flickity.cells.length; i++) {
+    flickity.cells[i].element.style.transform = normal;
+    flickity.cells[i].element.style.zIndex = ('1');
+  }
+  flickity.selectedElement.style.zIndex = '2';
+  if (flickity.selectedElement.getAttribute('layout') === 'split') {
+    flickity.selectedElement.style.transform = zoomed + ' rotate(90deg)';
+    return;
+  }
+  flickity.selectedElement.style.transform = zoomed;
 }
