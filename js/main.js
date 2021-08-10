3a9e7f94e5c9c8064d2818b3080442e9093fce02
humbertovnavarro <humberto.navarro.dev@gmail.com>
2021-08-09 23:31:03 -0700

    hotfix, fixed QR code url

--- js/main.js
+++ js/main.js
@@ -316,7 +316,7 @@ function switchView(string) {
     $deleteModal.parentElement.classList.remove('hidden');
   }
   if (string === 'qr') {
-    var url = 'https://api.qrserver.com/v1/create-qr-code/?data=https://humbertovnavarro.github.io/ajax-project/?';
+    var url = 'https://api.qrserver.com/v1/create-qr-code/?data=https://humbertovnavarro.github.io/majax/?';
     url += Deck.getActiveDeck().serialize();
     $qrImage.src = url;
     $qrModal.classList.remove('hidden');
